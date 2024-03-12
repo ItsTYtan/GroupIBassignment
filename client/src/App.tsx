b43451f65dae940bb493db7fbb5d8e139ca81d4f
ItsTYtan <tytan216@gmail.com>
2024-03-12 19:13:52 +0800

    Test commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,23 @@
+import './styles.css';
+
 const App = () => {
+    const evtSource = new EventSource('http://localhost:9000/events');
+
+    // evtSource.onmessage = (event) => {
+    //     const newElement = document.createElement("li");
+    //     const eventList = document.getElementById("list");
+      
+    //     newElement.textContent = `message: ${event.data}`;
+    //     eventList!.appendChild(newElement);
+    // }
+
     return (
-        <h1>Write your code here!</h1>
+        <>
+            <h1>Write your code here!</h1>
+            <ul id='list'>
+
+            </ul>
+        </>
     );
 };
 
